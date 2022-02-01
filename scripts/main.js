c99a72e40bae4aed7dabacefcb7764b3e1d6c529
Adachi Masakazu <marscode48@gmail.com>
2022-02-01 12:46:23 +0900

    main.jsでハンバーガーメニューを作成

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,46 @@
 jQuery
 ===================================================*/
 
+// $(function() {
+//   // ハンバーガーメニューをクリック時
+//   $('.hamburger').on('click', function() {
+//     hamburger();
+//   });
+//   メニューのリンクをクリック時
+//   $('#navi a').on('click', function() {
+//     hamburger();
+//   });
+// });
+
+// function hamburger() {
+//   $('.hamburger').toggleClass('active');
+
+//   if ($('.hamburger').hasClass('active')) {
+//     $('#navi').addClass('active');
+//   } else {
+//     $('#navi').removeClass('active');
+//   }
+// }
 
 /*=================================================
 JavaScript
 ===================================================*/
+
+// ハンバーガーメニュー
+const hamburger = document.querySelector('.hamburger');
+// ナビメニュー
+const navi = document.querySelector('#navi');
+
+hamburger.addEventListener('click', menuOpen);
+
+navi.addEventListener('click', menuOpen);
+
+function menuOpen() {
+  hamburger.classList.toggle('active');
+
+  if(hamburger.classList.contains('active')) {
+    navi.classList.add("active");
+  } else {
+    navi.classList.remove('active');
+  }
+}
